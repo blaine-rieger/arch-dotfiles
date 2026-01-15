0f6402be01af6db87f4073bb0ae8526578a15211
blaine-rieger <50034479+blaine-rieger@users.noreply.github.com>
2026-01-15 12:22:50 -0800

    Add files via upload

---
+++ nvim/lua/options.lua
@@ -0,0 +1,11 @@
+require "nvchad.options"
+
+vim.opt.tabstop = 4         -- A TAB character will look like 4 spaces
+vim.opt.shiftwidth = 4      -- Indentation will use 4 spaces
+vim.opt.expandtab = true    -- Pressing TAB inserts spaces instead of a TAB character
+vim.opt.softtabstop = 4     -- Number of spaces to insert/delete with TAB/backspace
+
+-- add yours here!
+
+-- local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
